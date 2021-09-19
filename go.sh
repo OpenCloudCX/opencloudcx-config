669517d5d7d61f9ee6f9fe8d57f8aa38b65290e2
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-19 17:17:48 -0400

    First pass at total configure

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Add jenkins jobx
-# terraform init
-# terraform apply -var "jenkins_url=http://$INGRESS_ENDPOINT" --var "github_username=ajnriva" --var "github_secret=$GITHUB_ACCESS_TOKEN" --var "jenkins_secret=$JENKINS_SECRET" --auto-approve 
+terraform init
+terraform apply -var "jenkins_url=http://$INGRESS_ENDPOINT" --var "github_username=ajnriva" --var "github_secret=$GITHUB_ACCESS_TOKEN" --var "jenkins_secret=$JENKINS_SECRET" --auto-approve 
 
 ##### kubectl file setup with commands
 echo "EKS_NAME --> $EKS_NAME"
@@ -43,4 +43,5 @@ echo " --------- TEST COMMAND"
 kubectl get pods -A
 
 ##### kubectl hal commands
-# k exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins enable"
+kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins enable"
+kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins master add jenkins --address $INGRESS_ENDPOINT --username admin --password $JENKINS_ADMIN_TOKEN"
