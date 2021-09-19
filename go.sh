59fc77cfbb1371cb9ddc13cf7853060dd2accaf8
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-19 11:55:26 -0400

    Using different parameter

--- go.sh
+++ go.sh
@@ -5,9 +5,9 @@ terraform init
 terraform apply -var "jenkins_url=http://$INGRESS_ENDPOINT" --var "github_username=ajnriva" --var "github_secret=$GITHUB_ACCESS_TOKEN" --var "jenkins_secret=$JENKINS_SECRET" --auto-approve 
 
 ##### kubectl file setup with commands
-echo "EKS_ENDPPOINT --> $EKS_ENDPOINT"
+echo "EKS_NAME --> $EKS_NAME"
 
-aws eks --region us-east-1 update-kubeconfig --name "$EKS_ENDPOINT"
+aws eks --region us-east-1 update-kubeconfig --name "$EKS_NAME"
 
 ##### kubectl hal commands
 # k exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins enable"
