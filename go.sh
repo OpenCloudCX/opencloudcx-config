4cd8494b847840081d44f866a8a7a2efd9ce6f5f
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-19 18:09:24 -0400

    Commented hal commands

--- go.sh
+++ go.sh
@@ -43,6 +43,6 @@ echo " --------- TEST COMMAND"
 kubectl get pods -A
 
 ##### kubectl hal commands
-kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins enable"
-kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins master add jenkins --address $INGRESS_ENDPOINT --username admin --password $JENKINS_ADMIN_TOKEN"
-kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal deploy apply"
+# kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins enable"
+# kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal config ci jenkins master add k8s-jenkins --address $INGRESS_ENDPOINT --username admin --password $JENKINS_ADMIN_TOKEN"
+# kubectl exec -it -n spinnaker spinnaker-spinnaker-halyard-0 -- bash -c "hal deploy apply"
