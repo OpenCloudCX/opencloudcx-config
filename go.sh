96fa9f27aaeac891f1c33215f046ae4af0486237
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-19 11:31:49 -0400

    Updated command line parameters

--- go.sh
+++ go.sh
@@ -2,7 +2,7 @@
 
 ##### Add jenkins jobx
 terraform init
-terraform apply -var "jenkins_url=$INGRESS_ENDPOINT " --auto-approve 
+terraform apply -var "jenkins_url=$INGRESS_ENDPOINT" --var "github_username=ajnriva" --var "github_secret=$GITHUB_ACCESS_TOKEN" --var "jenkins_secret=$JENKINS_SECRET" --auto-approve 
 
 ##### kubectl file setup with commands
 
